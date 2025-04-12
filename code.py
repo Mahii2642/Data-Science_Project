ad053d2b1497e72613ee87a96d39273a6cf9ad5c
Mahii2642 <152585665+Mahii2642@users.noreply.github.com>
2025-04-12 20:01:33 +0530

    Update code.py

--- code.py
+++ code.py
@@ -3,6 +3,7 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 
+
 # Load the dataset
 df = pd.read_csv("C:\\Users\\mailt\\Desktop\\Number of Schools by Management and Year of Establishment _Report type - State-wise_22 (2).csv")
 
